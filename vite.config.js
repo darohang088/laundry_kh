0c65331b0f31ed9885539d755b45c34895e74247
DaroHang <56570369+darohang088@users.noreply.github.com>
2026-03-31 13:56:03 +0700

    update UI

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/laundry_kh/",  // <-- MUST match your repo name exactly
+  base: "/laundry_kh/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
