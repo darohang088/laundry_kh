6e0afd730fa1f7174b897669487f057f6f0d4e48
DaroHang <56570369+darohang088@users.noreply.github.com>
2026-03-31 13:46:41 +0700

    update UI

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Laundrykh/", // <-- IMPORTANT: your repo name
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
